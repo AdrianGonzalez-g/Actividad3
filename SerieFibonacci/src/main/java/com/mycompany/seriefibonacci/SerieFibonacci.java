a248225530537b1f55d0e8976301106a603f42fb
AdrianGonzalez-g <DAM115@IF01-12.iesmhp.local>
2025-12-04 15:51:53 +0100

    Nuevo programa fibonacci

---
+++ SerieFibonacci/src/main/java/com/mycompany/seriefibonacci/SerieFibonacci.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.seriefibonacci;
+
+/**
+ *
+ * @author DAM115
+ */
+public class SerieFibonacci {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
